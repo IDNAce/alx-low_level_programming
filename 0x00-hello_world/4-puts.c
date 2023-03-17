9c6817ff86b8d21e6025e4ea090df8407f8b1e7d
IDNAce <omobawilliamjoshua@gmail.com>
2023-03-16 18:26:56 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /*
- * main - This is the main functio
- * n
+ * main - This is the main function
+ * 
  * Return: always 0
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
