e95ea12a2ef38a532d298d36f98990f8d956c1fe
IDNAce <omobawilliamjoshua@gmail.com>
2023-03-16 19:17:11 -0700

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - This is the main function
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+printf("int: 2bytes"\n);
+("char: 0.5bytes"\n);
+("float; 2bytes"\n);
+return (0);
+}
