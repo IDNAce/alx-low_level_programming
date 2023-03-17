93205fcc9952181d547abd5a133cdc5805a5e07b
IDNAce <omobawilliamjoshua@gmail.com>
2023-03-16 21:12:13 -0700

    Sizes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-fputc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 stdout;
 return (1);
 }
