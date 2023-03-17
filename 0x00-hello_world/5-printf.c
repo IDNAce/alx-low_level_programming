416f5f2aa7b926226ef41485f1d43640cc767e08
IDNAce <omobawilliamjoshua@gmail.com>
2023-03-16 18:58:37 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - This is the main program
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
