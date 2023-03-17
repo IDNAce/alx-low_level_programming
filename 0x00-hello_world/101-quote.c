a1d0c23beb948224e83e3db59e17b2ebddc2991c
IDNAce <omobawilliamjoshua@gmail.com>
2023-03-16 22:11:50 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  *  main - This is the main function
- *
  *  Return: 1 if successful
  */
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-stdout;
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
