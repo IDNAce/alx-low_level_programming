c182bf043208133593e02e3492a065abd0427287
IDNAce <omobawilliamjoshua@gmail.com>
2023-03-17 16:13:08 -0700

    Upper and lowercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
